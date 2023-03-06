450757327d010083ec1ba19f6a3e6cc41c3adf4d
DavidShen2 <81164854+ds12a@users.noreply.github.com>
2023-03-06 22:30:21 +0000

    debug script

--- api/static/script.js
+++ api/static/script.js
@@ -9,6 +9,11 @@ function displayConversation(formData) {
     document.getElementById("conversation").innerHTML = html + "<br>";
 }
 
+window.onerror = function(msg, url, linenumber) {
+    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    return true;
+}
+
 particlesJS.load('particles-js', './static/particlesjs-config.json', function() {
       console.log('callback - particles.js config loaded');});
       
