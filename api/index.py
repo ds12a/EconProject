0cfe15af97bb4693fe8d3f411024c4e36d3d95a0
DavidShen2 <81164854+ds12a@users.noreply.github.com>
2023-03-10 01:34:30 +0000

    Auth

--- api/index.py
+++ api/index.py
@@ -1,13 +1,22 @@
+import os
 from flask import Flask, render_template, request, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+#CONNECTION_STRING = os.environ['CONNECTION_STRING']
+
+#db = MongoClient(CONNECTION_STRING)['Accounts']
 
 @app.route('/', methods=['GET', 'POST'])
 def index(newMsgs = []):
     if request.method == 'POST':
+        isInit = False
+        code = ""
         for content in request.form:
-            print(f"{content}, {request.form[content]}")
+            if content == "init": isInit = True
+            elif content == "code": code = request.form[content]
+            else: print(f"{content}, {request.form[content]}")
         return "This is the response!"
         
     elif request.method == 'GET':
--- api/static/script.js
+++ api/static/script.js
@@ -5,6 +5,28 @@ function addToConversation(reply) {
     if (reply[0] != "server") document.getElementById("conversation").innerHTML += "<br>";
 }
 
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 window.onerror = function(msg, url, linenumber) {
     alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
     return true;
@@ -14,3 +36,20 @@ particlesJS.load('particles-js', './static/particlesjs-config.json', function() 
       console.log('callback - particles.js config loaded');});
 
 
+let code = prompt("Enter your access code:");
+
+let loginInfo = new FormData();
+loginInfo.append("code", code);
+loginInfo.append("init", "yes");
+
+fetch('/', {   // assuming the backend is hosted on the same server
+                  method: 'POST',
+                  body: loginInfo,
+              }).then((response) => response.text()).then(function(text) {
+                  conversation.append("server", text);
+                  addToConversation(["server", text]);
+                  document.getElementById('prompt').disabled = false;
+                  document.getElementById('prompt').focus();
+                  window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+                  document.getElementById("particles-js").style.backgroundColor = "#000000";
+              });
